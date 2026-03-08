3fe9b0c825aae6b8316060da3ea19752c7198d5c
Lalitjindal-code <lalitjindal519@gmail.com>
2026-03-08 21:44:55 +0530

    refactor: use env variables for firebase config

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDvWBDKoou0g0w1LArmw5NWh0bDjyXeRJQ",
-    authDomain: "trusiq-6d9f5.firebaseapp.com",
-    projectId: "trusiq-6d9f5",
-    storageBucket: "trusiq-6d9f5.firebasestorage.app",
-    messagingSenderId: "1087651530501",
-    appId: "1:1087651530501:web:e7abdfb83c418fed02196c",
-    measurementId: "G-SR86SXMLXZ"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
